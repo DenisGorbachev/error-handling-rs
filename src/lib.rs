97cc65e88af9e25bd7f70b3e48c604fedd79ba5c
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-08-30 20:16:56 +0700

    feat: add guidelines

--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,93 @@
-/*!
-# Error handling
-
-## Goal
-
-The error handling code should help the caller to diagnose the issue, fix it, and retry the call.
-
-## Approach
-
-Every error must be represented by a unique enum variant with relevant data.
-
-## Notes
-
-Some arguments that have been passed by value may already be unavailable when a specific fallible expression is executed:
-
-```rust
-fn foo(a: String, b: String) {
-    let a_new = bar(a);
-    if b.is_empty() {
-        // NOTE: `a` is unavailable here because it has been consumed by `bar`
-        todo!()
-    }  else {
-        todo!()
-    }
-}
-
-fn bar(a: String) -> String {
-    todo!()
-}
-```
-
-When calling a fallible function on each element of a collection, it is better to keep all Results instead of short-circuiting on the first error.
-
-Wrap secret data in a type that doesn't output the secret data in the Debug and Display impls (e.g. [`secrecy::SecretBox`])
-Keep the Copy types
-Keep the relevant vars (passed by reference)
-Note that some vars that were received by value may have already been moved into another function to produce another intermediate value; these can't be returned anymore - so we must always return an enum variant because different values will be available at different points
-Handling Option types requires another macro (handle_opt!)
-
-## Definitions
-
-### Fallible expression
-
-An expression that returns a [`Result`].
-
-For example:
-
-```rust
-use error_handling::{handle, handle_bool};
-use derive_more::{Error, Display};
-
-pub fn foo(numbers: Vec<u32>) -> Result<u32, FooError> {
-    use FooError::*;
-    // the following `if` is a fallible expression
-    if numbers.is_empty() {
-        return Err(NumbersAreEmpty { numbers });
-    }
-    // the following call to `find_even` is a fallible expression
-    let result = find_even(numbers.clone().into_iter());
-    let even = result.map_err(|source| FindEven { source })?;
-    Ok(even)
-}
-
-pub fn find_even(mut numbers: impl Iterator<Item = u32>) -> Result<u32, FindEvenError> {
-    use FindEvenError::*;
-    numbers.find(|x| x % 2 == 0).ok_or(NotFound)
-}
-
-#[derive(Error, Display, Debug)]
-pub enum FooError {
-    #[display("Numbers are empty: {numbers:#?}")]
-    NumbersAreEmpty { numbers: Vec<u32> },
-    FindEven { source: FindEvenError }
-}
-
-#[derive(Error, Display, Debug)]
-pub enum FindEvenError {
-    NotFound
-}
-```
-
-*/
+//! # Error handling
+//!
+//! ## Goal
+//!
+//! Help the caller diagnose the issue, fix it, and retry the call.
+//!
+//! ## Approach
+//!
+//! Every error must be represented by a unique enum variant with relevant fields.
+//!
+//! ## Guidelines
+//!
+//! * Every fallible function must return a unique error type
+//! * The error type must be an enum
+//! * The name of the error enum should answer "what" failed, and its variants should answer "why" it failed
+//! * The name of the error type must end with `Error`
+//! * The name of the error type must include the name of the function converted to CamelCase
+//!   * If the function is a freestanding function, the name of the error type must be exactly equal to the name of the function converted to CamelCase concatenated with `Error`
+//!   * If the function is an associated function, the name of the error type must be exactly equal to the name of the type without generics concatenated with the name of the function in CamelCase concatenated with `Error`
+//!   * If the error is specified as an associated type of a foreign trait with multiple functions that return this associated error type, then the name of the error type must be exactly equal to the name of the trait including generics concatenated with the name of the type for which this trait is implemented concatenated with `Error`
+//!
+//! ## Notes
+//!
+//! Some arguments that have been passed by value may already be unavailable when a specific fallible expression is executed:
+//!
+//! ```rust
+//! fn foo(a: String, b: String) {
+//!     let a_new = bar(a);
+//!     if b.is_empty() {
+//!         // NOTE: `a` is unavailable here because it has been consumed by `bar`
+//!         todo!()
+//!     }  else {
+//!         todo!()
+//!     }
+//! }
+//!
+//! fn bar(a: String) -> String {
+//!     todo!()
+//! }
+//! ```
+//!
+//! When calling a fallible function on each element of a collection, it is better to keep all Results instead of short-circuiting on the first error.
+//!
+//! Wrap secret data in a type that doesn't output the secret data in the Debug and Display impls (e.g. [`secrecy::SecretBox`])
+//! Keep the Copy types
+//! Keep the relevant vars (passed by reference)
+//! Note that some vars that were received by value may have already been moved into another function to produce another intermediate value; these can't be returned anymore - so we must always return an enum variant because different values will be available at different points
+//! Handling Option types requires another macro (handle_opt!)
+//!
+//! ## Definitions
+//!
+//! ### Fallible expression
+//!
+//! An expression that returns a [`Result`].
+//!
+//! For example:
+//!
+//! ```rust
+//! use error_handling::{handle, handle_bool};
+//! use derive_more::{Error, Display};
+//!
+//! pub fn foo(numbers: Vec<u32>) -> Result<u32, FooError> {
+//!     use FooError::*;
+//!     // the following `if` is a fallible expression
+//!     if numbers.is_empty() {
+//!         return Err(NumbersAreEmpty { numbers });
+//!     }
+//!     // the following call to `find_even` is a fallible expression
+//!     let result = find_even(numbers.clone().into_iter());
+//!     let even = result.map_err(|source| FindEven { source })?;
+//!     Ok(even)
+//! }
+//!
+//! pub fn find_even(mut numbers: impl Iterator<Item = u32>) -> Result<u32, FindEvenError> {
+//!     use FindEvenError::*;
+//!     numbers.find(|x| x % 2 == 0).ok_or(NotFound)
+//! }
+//!
+//! #[derive(Error, Display, Debug)]
+//! pub enum FooError {
+//!     #[display("Numbers are empty: {numbers:#?}")]
+//!     NumbersAreEmpty { numbers: Vec<u32> },
+//!     FindEven { source: FindEvenError }
+//! }
+//!
+//! #[derive(Error, Display, Debug)]
+//! pub enum FindEvenError {
+//!     NotFound
+//! }
+//! ```
 
 /// [`handle!`](crate::handle) is a better alternative to [`map_err`](Result::map_err) because it doesn't capture any variables from the environment if the result is [`Ok`], only when the result is [`Err`].
 /// By contrast, a closure passed to `map_err` always captures the variables from environment, regardless of whether the result is [`Ok`] or [`Err`]
@@ -135,13 +143,12 @@ macro_rules! handle_bool {
 
 #[cfg(test)]
 mod tests {
-    #![allow(dead_code)]
-
     use super::*;
     use derive_more::{Display, Error};
 
     #[test]
     fn must_handle_opt() {
+        #[allow(dead_code)]
         fn find_even(numbers: Vec<u32>) -> Result<u32, FindEvenError> {
             use FindEvenError::*;
             let even = handle_opt!(numbers.iter().find(|x| *x % 2 == 0), NotFound);

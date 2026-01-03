ffd7d59d152244850ad08a455c327335da02e134
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-01-03 13:01:34 +0700

    fix: implement generic ErrVec

--- src/functions/fmt.rs
+++ src/functions/fmt.rs
@@ -1,12 +1,47 @@
+use crate::Errgonomic;
+use core::fmt::Display;
 use core::fmt::Formatter;
+use core::fmt::Result;
 
 /// Formats the `error` using the [auto-deref trick](https://lukaskalbertodt.github.io/2019/12/05/generalized-autoref-based-specialization.html):
 ///
-/// - If the `error` implements [`Errgonomic`](crate::Errgonomic), formats it using [`Errgonomic::fmt`](crate::Errgonomic::fmt)
-/// - If the `error` implements [`Display`](std::fmt::Display), formats it using [`Display::fmt`](std::fmt::Display::fmt)
-/// - Else returns a [formatting error](std::fmt::Error)
+/// - If the `error` implements [`Errgonomic`], formats it using [`Errgonomic::fmt`]
+/// - If the `error` implements [`Display`](Display), formats it using [`Display::fmt`](Display::fmt)
+/// - Else returns a [formatting error](core::fmt::Error)
 ///
 /// Note that if the `error` implements `Error`, then it also implements `Display` (it's a supertrait of `Error`).
-pub fn fmt<E>(_error: E, _formatter: &mut Formatter<'_>) -> core::fmt::Result {
-    todo!()
+#[allow(clippy::needless_borrow, clippy::needless_borrows_for_generic_args)]
+pub fn fmt<E>(error: E, formatter: &mut Formatter<'_>) -> Result {
+    let wrapped = AutoFmt(error);
+    (&&&wrapped).fmt(formatter)
+}
+
+struct AutoFmt<T>(T);
+
+impl<T: Errgonomic> Errgonomic for &&AutoFmt<T> {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
+        Errgonomic::fmt(&self.0, formatter)
+    }
+}
+
+// #[doc(hidden)]
+// pub trait ViaDisplay {
+//     fn fmt(&self, formatter: &mut Formatter<'_>) -> core::fmt::Result;
+// }
+
+impl<T: Display> Display for &AutoFmt<T> {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
+        Display::fmt(&self.0, formatter)
+    }
+}
+
+#[doc(hidden)]
+pub trait Fallback {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result;
+}
+
+impl<T> Fallback for AutoFmt<T> {
+    fn fmt(&self, _formatter: &mut Formatter<'_>) -> Result {
+        Err(core::fmt::Error)
+    }
 }
--- src/types/err_vec.rs
+++ src/types/err_vec.rs
@@ -1,50 +1,103 @@
-use std::error::Error;
-use std::fmt::{Display, Formatter};
-use std::ops::{Deref, DerefMut};
+use crate::writeln_error_to_formatter;
+use core::error::Error;
+use core::fmt::Debug;
+use core::fmt::{Display, Formatter};
+use core::ops::{Deref, DerefMut};
 
 /// An owned collection of errors that itself implements [`Error`].
+/// T must implement Display or Errgonomic.
 #[derive(Default, Debug)]
-pub struct ErrVec {
-    /// Collected errors stored as boxed trait objects.
-    pub inner: Vec<Box<dyn Error + 'static>>,
+pub struct ErrVec<E> {
+    /// Collected errors.
+    pub inner: Vec<E>,
 }
 
-impl Display for ErrVec {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("encountered {len} errors", len = self.inner.len()))
+impl<E: Error> Display for ErrVec<E> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        writeln!(f, "encountered {len} errors", len = self.len())?;
+        self.inner.iter().try_for_each(|error| {
+            let recursive_displayer = RecursiveDisplayer {
+                error,
+                is_nested: true,
+            };
+            let string = format!("{recursive_displayer}");
+            let mut lines = string.lines();
+            let first_line_opt = lines.next();
+            if let Some(first_line) = first_line_opt {
+                writeln!(f, "  * {first_line}")?;
+                lines.try_for_each(|line| writeln!(f, "    {line}"))?;
+                writeln!(f)?;
+            }
+            Ok(())
+        })
     }
 }
 
-impl Error for ErrVec {}
+pub struct RecursiveDisplayer<'a, E: ?Sized> {
+    pub error: &'a E,
+    pub is_nested: bool,
+}
 
-impl Deref for ErrVec {
-    type Target = Vec<Box<dyn Error + 'static>>;
+impl<'a, E: Error + ?Sized> Display for RecursiveDisplayer<'a, E> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        writeln_error_to_formatter(self.error, self.is_nested, f)
+    }
+}
+
+impl<'a, E: Error + ?Sized> From<&'a E> for RecursiveDisplayer<'a, E> {
+    fn from(error: &'a E) -> Self {
+        Self {
+            error,
+            is_nested: false,
+        }
+    }
+}
+
+impl<E: Error> Error for ErrVec<E> {}
+
+impl<E> Deref for ErrVec<E> {
+    type Target = Vec<E>;
 
     fn deref(&self) -> &Self::Target {
         &self.inner
     }
 }
 
-impl DerefMut for ErrVec {
+impl<E> DerefMut for ErrVec<E> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.inner
     }
 }
 
-impl ErrVec {
+impl<E> ErrVec<E> {
     /// Builds an [`ErrVec`] by boxing each error from the iterator.
-    pub fn new<E: Error + 'static>(iter: impl IntoIterator<Item = E>) -> Self {
+    pub fn new(iter: impl IntoIterator<Item = E>) -> Self {
         Self {
-            inner: iter
-                .into_iter()
-                .map(|err| Box::new(err) as Box<dyn Error + 'static>)
-                .collect(),
+            inner: iter.into_iter().collect(),
         }
     }
 }
 
-impl<E: Error + 'static> From<Vec<E>> for ErrVec {
-    fn from(value: Vec<E>) -> Self {
-        Self::new(value)
+impl<E> From<Vec<E>> for ErrVec<E> {
+    fn from(inner: Vec<E>) -> Self {
+        Self {
+            inner,
+        }
+    }
+}
+
+impl<E: Clone, const N: usize> From<[E; N]> for ErrVec<E> {
+    fn from(inner: [E; N]) -> Self {
+        Self {
+            inner: inner.to_vec(),
+        }
+    }
+}
+
+impl<E: Clone> From<&[E]> for ErrVec<E> {
+    fn from(inner: &[E]) -> Self {
+        Self {
+            inner: inner.to_vec(),
+        }
     }
 }
